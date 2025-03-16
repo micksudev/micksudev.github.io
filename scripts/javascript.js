6e6694e0a4890cb7679d208e8422da3231bc91ed
micksudev <micksudev@gmail.com>
2025-03-16 23:14:29 +0700

    Updated: version 0.1.3

--- scripts/javascript.js
+++ scripts/javascript.js
@@ -2,6 +2,13 @@ const themeToggle = document.getElementById('theme-toggle');
 const themeIcon = document.getElementById('theme-icon');
 const linkedinIcon = document.getElementById('linkedin-icon');
 const emailIcon = document.getElementById('email-icon');
+const arrowRightIcon = document.getElementById('arrow-right');
+const arrowLeftIcon = document.getElementById('arrow-left');
+
+const homeContent = document.getElementById("homeContent");
+const aboutMeContent = document.getElementById("aboutMeContent");
+const homeBtn = document.getElementById("home-btn");
+const aboutMeBtn = document.getElementById("about-btn");
 
 const currentTheme = localStorage.getItem('theme');
 if (currentTheme) {
@@ -12,13 +19,17 @@ if (currentTheme) {
 
 function updateIcon() {
   if (document.body.classList.contains('dark-mode')) {
-    themeIcon.src = 'images/img-icon-sun.png';
-    linkedinIcon.src = "images/img-icon-linkedin-white.png"
-    emailIcon.src = "images/img-icon-email-white.png"
+    if(themeIcon) themeIcon.src = 'images/img-icon-sun.png';
+    if(linkedinIcon) linkedinIcon.src = "images/img-icon-linkedin-white.png"
+    if(emailIcon) emailIcon.src = "images/img-icon-email-white.png"
+    if(arrowRightIcon) arrowRightIcon.src = "images/img-arrow-right-black.png"
+    if(arrowLeftIcon) arrowLeftIcon.src = "images/img-arrow-left-white.png"
   } else {
-    themeIcon.src = 'images/img-icon-moon.png';
-    linkedinIcon.src = "images/img-icon-linkedin-black.png"
-    emailIcon.src = "images/img-icon-email-black.png"
+    if(themeIcon) themeIcon.src = 'images/img-icon-moon.png';
+    if(linkedinIcon) linkedinIcon.src = "images/img-icon-linkedin-black.png"
+    if(emailIcon) emailIcon.src = "images/img-icon-email-black.png"
+    if(arrowRightIcon) arrowRightIcon.src = "images/img-arrow-right-white.png"
+    if(arrowLeftIcon) arrowLeftIcon.src = "images/img-arrow-left-black.png"
   }
 }
 
@@ -39,12 +50,13 @@ themeToggle.addEventListener('click', (event) => {
 });
 
 //Button menu
-
-document.getElementById('home-btn').addEventListener('click', function() {
+if(homeBtn)
+{
+homeBtn.addEventListener('click', function() {
   toggleButton(this);
 });
 
-document.getElementById('about-btn').addEventListener('click', function() {
+aboutMeBtn.addEventListener('click', function() {
   toggleButton(this);
 });
 
@@ -59,11 +71,6 @@ function toggleButton(button) {
 
 
 //Home & AboutMe
-const homeContent = document.getElementById("homeContent");
-const aboutMeContent = document.getElementById("aboutMeContent");
-const homeBtn = document.getElementById("home-btn");
-const aboutMeBtn = document.getElementById("about-btn");
-
 homeBtn.addEventListener("click", () => {
 
   homeContent.classList.add("active");
@@ -75,13 +82,29 @@ aboutMeBtn.addEventListener("click", () => {
   aboutMeContent.classList.add("active");
   homeContent.classList.remove("active");
 });
+}
 
 // Profile
+const profileImage = document.getElementById("profileImage");
 
 document.querySelector('.myprofile').addEventListener('mouseover', function() {
-  document.getElementById('profileImage').src = 'images/img-profile2.png';
-});
+  if(profileImage) profileImage.src = 'images/img-profile2.png';
+}); 
 
 document.querySelector('.myprofile').addEventListener('mouseout', function() {
-  document.getElementById('profileImage').src = 'images/img-profile.png';
+  if(profileImage) profileImage.src = 'images/img-profile.png';
 });
+
+
+// Gallery
+function openModal(imgElement) {
+  const modal = document.getElementById("myModal");
+  const modalImg = document.getElementById("imgModal");
+  modal.style.display = "flex";
+  modalImg.src = imgElement.src;
+}
+
+function closeModal() {
+  const modal = document.getElementById("myModal");
+  modal.style.display = "none";
+}
